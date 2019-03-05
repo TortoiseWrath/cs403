c0b9935e9089d00d0874cc8cd85b228f003484eb
tortoise <sdgillen@crimson.ua.edu>
2019-03-05 13:51:25 -0600

    task8i

---
+++ assign1/task9r.scm
@@ -0,0 +1,13 @@
+(define (ramanujanr n)
+	((lambda (x) 
+		(* n n)
+	) (+ 1 n))
+)
+
+(define (main)
+	;(setPort (open (getElement ScamArgs 1) 'read))
+    ;(define n (readInt))
+	(define n (int (getElement ScamArgs 1)))
+	(println "(ramanujanr " n ") is " (fmt "%.25f" (ramanujanr n)))
+	(println "$4$")
+)
