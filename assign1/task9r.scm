7290bea3c8c7cfd798d88923bae7dd6fbcd6594f
tortoise <sdgillen@crimson.ua.edu>
2019-03-05 14:03:00 -0600

    task9r args

--- assign1/task9r.scm
+++ assign1/task9r.scm
@@ -13,9 +13,8 @@
 )
 
 (define (main)
-	;(setPort (open (getElement ScamArgs 1) 'read))
-    ;(define n (readInt))
-	(define n (int (getElement ScamArgs 1)))
+	(setPort (open (getElement ScamArgs 1) 'read))
+    (define n (readInt))
 	(println "(ramanujanr " n ") is " (fmt "%.25f" (real (ramanujanr n))))
 	(println "$4$")
 )
