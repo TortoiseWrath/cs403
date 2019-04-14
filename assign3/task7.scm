04bc5748740ef8cf1311b8c4164620d8e9956c9a
tortoise <sdgillen@crimson.ua.edu>
2019-04-14 16:06:53 -0500

    task7

--- assign3/task7.scm
+++ assign3/task7.scm
@@ -15,32 +15,54 @@
 (define (mystery x)
 	(define (mys i num denom)
 		(define n (* num x x))
-		(define d (* denom (* i 2) (- (* i 2) 1))
+		(define d (* denom (* i 2) (- (* i 2) 1)))
+		(if (= 0 (% i 2))
+			(define sign 1)
+			(define sign -1)
+		)
 		(cons-stream
-			(* 
-				(if (= 0 (% i 2)) 1 -1)
-				(/ n d)
-			)
+			(* sign (/ n d))
 			(mys (+ i 1) n d)
 		)
 	)
-	(cons-stream 1 (mys 1 1 1))
+	(cons-stream 1.0 (mys 1 1.0 1.0))
 )
 
 (define (ps-mystery x)
+	(define (ps s prev)
+		(define sum (+ (stream-car s) prev))
+		(cons-stream sum (ps (stream-cdr s) sum))
+	)
+	(ps (mystery x) 0.0)
+)
 
+(define (euler-transform s) 
+	(define (square x) (* x x))
+	(define s0 (stream-car s))
+	(define s1 (stream-car (stream-cdr s)))
+	(define s2 (stream-car (stream-cdr (stream-cdr s))))	
+	(if (= s1 s2)
+		(cons-stream s1 (euler-transform (stream-cdr s)))
+		(cons-stream
+			(- s2 (/ (square (- s2 s1)) (+ s0 (* -2 s1) s2)))
+			(euler-transform (stream-cdr s))
+		)
+	)
 )
 
 (define (acc-mystery x)
-
+	(euler-transform (ps-mystery x))
 )
 
 (define (super-mystery x)
-
+	(define (superaccel s)
+		(cons-stream (stream-car s) (superaccel (euler-transform (stream-cdr s))))
+	)
+	(superaccel (ps-mystery x))
 )
 
 (define (symbolic-mystery)
-	(println "i dunno")
+	(println "(mystery x) is $\\cos x$")
 )
 
 (define (main)
